508399da7c0579a1723afafc32409c2b8dec0b4d
tuomsa <tuomashiljanen@gmail.com>
2024-02-04 17:03:48 +0200

    Tank rotation towards mouse

--- TanksGame/Assets/TankMaterials/Movement.cs
+++ TanksGame/Assets/TankMaterials/Movement.cs
@@ -12,14 +12,17 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         // Get input from arrow keys
-        float horizontalInput = -Input.GetAxis("Horizontal");
-        float verticalInput = -Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
         // Calculate movement direction
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput).normalized;
 
         // Move the player
         transform.Translate(movement * speed * Time.deltaTime);
+
+        // Rotate player towards the mouse
+        RotateTowardsMouse();
 
         if (Input.GetButtonDown("Fire1")) // Assuming "Fire1" is the left mouse button
         {
@@ -43,4 +46,25 @@ public class PlayerMovement : MonoBehaviour
             }
         }
     }
+
+    void RotateTowardsMouse()
+{
+    // Get the mouse position in screen coordinates
+    Vector3 mousePosition = Input.mousePosition;
+
+    // Convert the mouse position to a ray from the camera
+    Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+
+    // Perform a raycast to get the point where the ray intersects with the plane at the player's position
+    Plane plane = new Plane(Vector3.up, transform.position);
+    float rayDistance;
+    if (plane.Raycast(ray, out rayDistance))
+    {
+        // Get the point where the ray intersects with the plane
+        Vector3 targetPoint = ray.GetPoint(rayDistance);
+
+        // Rotate the player towards the target point
+        transform.LookAt(new Vector3(targetPoint.x, transform.position.y, targetPoint.z));
+    }
+}
 }
